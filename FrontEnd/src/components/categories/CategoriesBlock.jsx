82138e11af887e394e2981d5f6a8d31295facee1
Thairy <thairydaza@gmail.com>
2021-12-16 10:39:59 -0300

    category component

---
+++ FrontEnd/src/components/categories/CategoriesBlock.jsx
@@ -0,0 +1,29 @@
+import CategoryCard from "./CategoryCard";
+import "./CategoriesBlock.css";
+import useFetch from "../../hooks/useFetch";
+
+export default function CategoriesBlock() {
+ // const URL_API = "http://localhost:8080/API/category";
+  const URL_API = "http://digitalbooking.click:8080/API/category";
+
+  const { data } = useFetch(URL_API, {});
+
+  return data ? (
+    <section className="PrincipalContainer">
+      <div className="CategoriesSection">
+        <h2 className="TextCategory">Buscar por tipo de alojamiento</h2>
+
+        {data.map((category) => {
+          return (
+            <CategoryCard
+              key={category.id}
+              img={category.imageURL}
+              title={category.name}
+              amount="807.105"
+            />
+          );
+        })}
+      </div>
+    </section>
+  ) : null;
+}
