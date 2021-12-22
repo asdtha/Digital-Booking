395dd8d270a2a77f21db331063be5458421ab10e
Thairy <thairydaza@gmail.com>
2021-12-22 10:58:16 -0300

    container

---
+++ FrontEnd/src/container/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+}
+
+export default App;
