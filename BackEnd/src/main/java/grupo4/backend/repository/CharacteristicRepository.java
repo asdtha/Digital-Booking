65ca5fca8d9cb5141804985335a351ea93a8e804
Thairy <thairydaza@gmail.com>
2021-12-28 18:43:26 -0300

    Characteristic repository

---
+++ BackEnd/src/main/java/grupo4/backend/repository/CharacteristicRepository.java
@@ -0,0 +1,10 @@
+package grupo4.backend.repository;
+import grupo4.backend.model.Characteristic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacteristicRepository extends JpaRepository<Characteristic, Long>{
+
+}
+
