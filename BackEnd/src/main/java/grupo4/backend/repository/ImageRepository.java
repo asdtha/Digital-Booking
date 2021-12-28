bb0ff2065bf0eddca8dd1691612006681962bf60
Thairy <thairydaza@gmail.com>
2021-12-28 19:53:50 -0300

    Image repository

---
+++ BackEnd/src/main/java/grupo4/backend/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package grupo4.backend.repository;
+import grupo4.backend.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long>{
+
+}
+
